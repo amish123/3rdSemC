9c242866e3e4ecbe8ca94b9749a937d0e3ead6e6
MohMaya <shivanshuchaudhary97@gmail.com>
2016-12-14 22:54:52 +0530

    Rectified the helical program

--- P01.c
+++ P01.c
@@ -1,67 +1,35 @@
-#include<stdio.h>
-#include<math.h>
-void rf(int,int,int);
-void rb(int,int,int);
-void cu(int,int,int);
-void cd(int,int,int);
-int a[100][100];
-int flag=0;
+#include <stdio.h>
+#include <stdlib.h>
 int main()
-{int b,c,c2,d,e,e2,f,g,g2,h,j,j2,i,n;
-	printf("enter the size of the 2d array");
-	scanf("%d",&n);
-	a[n][n];
-	b=0;c=0;c2=n-1;d=n-1;e=1;e2=n-1;f=n-1;g=n-2;g2=0;h=0;j=n-2;j2=1;
-	while(1)
-	{if(flag==n*n)
-	break;
-		rf(b,c,c2);
-		cd(d,e,e2);
-		rb(f,g,g2);
-		cu(h,j,j2);
-		b=b+1;c=c+1;c2=c2-1;
-	d=d-1;e=e+1;e2=e2-1;
-	f=f-1;g=g-1;g2=g2+1;
-	h=h+1;j=j-1;j2=j2+1;
-		
-		}
-		for(i=0;i<n;i++)
-		{
-		for(j=0;j<n;j++)
-		printf("%d  ",a[i][j]);
-printf("\n");
+{
+    int r,c,i,j;
+    int arr[100][100];
+    printf("\n\nEnter the no. of rows :");
+    scanf("%d",&r);
+    printf("\n\nEnter the no. of columns :");
+    scanf("%d",&c);
+    printf("\n\nEnter the elements\n\n");
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+            scanf("%d",&arr[i][j]);
+    }
+    int rmin=0,cmin=0,counter=0,cmax=r*c;r--;c--;
+    printf("\n\nFinal Output\n");
+    while(counter<=cmax)
+    {
+      for(i=cmin;i<=c&&counter<=cmax;i++)
+      printf(" % d",arr[rmin][i]);
+    rmin++;
+    for(i=rmin;i<=r&&counter<=cmax;i++)
+        printf(" %d ",arr[i][c]);
+    c--;
+     for(i=c;i>=cmin&&counter<=cmax;i--)
+        printf(" %d ",arr[r][i]);
+    r--;
+     for(i=r;i>=rmin&&counter<=cmax;i--)
+        printf(" %d ",arr[i][cmin]);
+    cmin++;
+    }
+    return 0;
 }
-}
-void rf(int b,int c,int c2)
-{int i;
-	for(i=c;i<=c2;i++)
-	{
-	scanf("%d",&a[b][i]);
-++flag;
-}
-}
-void cd(int d,int e,int e2)
-{int i;
-	for(i=e;i<=e2;i++)
-	{
-	scanf("%d",&a[i][d]);
-	++flag;
-}
-}
-void rb(int f,int g,int g2)
-{int i;
-	for(i=g;i>=g2;i--)
-	{
-	scanf("%d",&a[f][i]);
-	++flag;
-}
-}
-void cu(int h,int j,int j2)
-{int i;
-	for(i=j;i>=j2;i--)
-	{
-	scanf("%d",&a[i][h]);
-++flag;
-}
-}
-
